e4f3782e043b8fb05e7d40a26570ca8ca3ad9bc5
BytePrashant <prashanttomar765@gmail.com>
2023-10-18 15:35:21 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const userRouter = require("./routes/user");
 
 dotenv.config();
 const app = express();
-
 app.use(cors());
+
 app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
